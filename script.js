f6576e89c0e9cc22b91289fbd932824eaff1a038
Martin <martin.kerek@gmail.com>
2018-10-28 12:10:11 +0100

    ok

--- script.js
+++ script.js
@@ -1,26 +1,2 @@
-//Excercise 1,2
-let myVariable = 4;
-console.log(myVariable * 8);
-console.log(myVariable);
-console.log("hello to my schooolmates");
-//Concatenate
-let name = "martin";
-let surname = "kerek";
-let names = ["martin","michal","milan"];
-
-console.log(names);
-console.log(`${name} ${surname}`);
-//If statement
-if (name) { console.log(surname); }
-else { console.log(name); }
-//Swap variables
-let a="martin";
-let b="michal";
-let c;
-
-c=a;
-a=b;
-b=c;
-console.log(a);
-console.log(b);
-
+firebase.initializeApp(config);
+fb = firebase.database();
